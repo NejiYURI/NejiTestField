addb5737a9302bd44ba69f8d28a05e1e84da292b
NejiYURI <halo010354@gmail.com>
2022-12-28 21:23:37 -0700

    Fix self-made tile system bug

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -18,6 +18,8 @@ public class TileManager : MonoBehaviour
 
     private Isometric_Matrix GirdMatrix = new Isometric_Matrix();
 
+    private float Timer;
+
     private void Awake()
     {
         GridMap = new Dictionary<Vector2?, GameObject>();
@@ -25,7 +27,24 @@ public class TileManager : MonoBehaviour
     }
     void Start()
     {
+        Timer = 0;
+    }
+    private void FixedUpdate()
+    {
+        Timer += Time.deltaTime;
+        for (int i = 0; i < Row; i++)
+        {
+            for (int j = 0; j < Col; j++)
+            {
+                if (HasTile(new Vector2(i, j)))
+                {
+                    GameObject obj = GridMap[new Vector2(i, j)];
+                    float ypos = Mathf.Sin(Timer * 10f + i+j) * 0.02f;
+                    obj.transform.position += new Vector3(0, ypos, 0);
+                }
+            }
 
+        }
     }
 
     public Vector2? GetTileVector(int index)
@@ -35,7 +54,7 @@ public class TileManager : MonoBehaviour
         {
             if (count != index)
             {
-                count++; 
+                count++;
                 continue;
             }
             return item.Key;
@@ -81,7 +100,8 @@ public class TileManager : MonoBehaviour
                 Transform SpawnBase = this.transform;
                 if (SpawnTarget != null)
                     SpawnBase = SpawnTarget;
-                Vector2 pos = ToScreenVector(new Vector2(x, y), TileSize);
+                Vector2 GridPos = new Vector2(x, y);
+                Vector2 pos = ToScreenVector(GridPos, TileSize);
                 pos += (Vector2)SpawnBase.position;
                 GameObject newTile = Instantiate(basicTile, pos, Quaternion.identity);
 
@@ -90,16 +110,16 @@ public class TileManager : MonoBehaviour
                 {
                     newTile.GetComponent<TileObject>().TileData = BaseTileData;
                     newTile.GetComponent<TileObject>().TileSet();
-                    newTile.name = "Tile[" + x + "," + y + "]";
+                    newTile.name = "Tile[" + x + "," + y + "][" + pos.x + "," + pos.y + "]";
                     newTile.transform.localScale = new Vector2(TileSize, TileSize);
                     if (SpawnTarget != null)
                         newTile.transform.SetParent(SpawnTarget);
                     else
                         newTile.transform.SetParent(this.transform);
-                    if (!GridMap.ContainsKey(pos))
+                    if (!GridMap.ContainsKey(GridPos))
                     {
-                        Debug.Log("Add Key " + pos);
-                        GridMap.Add(pos, newTile);
+                        Debug.Log("Add Key " + GridPos);
+                        GridMap.Add(GridPos, newTile);
                     }
 
                 }
@@ -153,20 +173,24 @@ public class TileManager : MonoBehaviour
         return (new Vector2(i_vector.x * GirdMatrix.i_hat.x + i_vector.y * GirdMatrix.j_hat.x, i_vector.x * GirdMatrix.i_hat.y + i_vector.y * GirdMatrix.j_hat.y)) * i_tilesize;
     }
 
-    private Vector2 ToGridVector(Vector2 i_vector, float i_tilesize = 1)
+    public Vector2 ToGridVector(Vector2 i_vector, float i_tilesize = 1)
     {
-        float a = GirdMatrix.i_hat.x * i_tilesize;
-        float b = GirdMatrix.j_hat.x * i_tilesize;
-        float c = GirdMatrix.i_hat.y * i_tilesize;
-        float d = GirdMatrix.j_hat.y * i_tilesize;
+        Transform SpawnBase = this.transform;
+        if (SpawnTarget != null)
+            SpawnBase = SpawnTarget;
+        i_vector -= (Vector2)SpawnBase.position;
+        float a = GirdMatrix.i_hat.x;
+        float b = GirdMatrix.j_hat.x;
+        float c = GirdMatrix.i_hat.y;
+        float d = GirdMatrix.j_hat.y;
         Isometric_Matrix inv = invert_matrix(a, b, c, d);
-        return (new Vector2(i_vector.x * inv.i_hat.x + i_vector.y * inv.i_hat.y, i_vector.x * inv.j_hat.x + i_vector.y * inv.j_hat.y)) * i_tilesize;
+        return (new Vector2(i_vector.x * inv.i_hat.x + i_vector.y * inv.j_hat.x, i_vector.x * inv.i_hat.y + i_vector.y * inv.j_hat.y)) * i_tilesize;
     }
 
     private Isometric_Matrix invert_matrix(float a, float b, float c, float d)
     {
-        float det = (1 / a * d - b * c);
-        return new Isometric_Matrix(new Vector2(det * d, det * -1f * b), new Vector2(det * -1f * c, det * a));
+        float det = (1 / (a * d - b * c));
+        return new Isometric_Matrix(new Vector2(det * d, det * -1f * c), new Vector2(det * -1f * b, det * a));
     }
 
 
